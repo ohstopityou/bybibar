9fc108885816ab3b78233da55c3d53159f70ada2
ohstopityou <thomasovik@gmail.com>
2017-02-27 00:58:42 +0100

    restored parks

--- js/maps.js
+++ js/maps.js
@@ -1,93 +1,99 @@
 function initMap() {
-  var uluru = {lat: 60.3907, lng: 5.322};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  center = {lat: 60.393, lng: 5.322};
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: uluru,
+    center: center,
     zoomControl: false,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
     styles: [
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels",
-    "stylers": [
       {
-        "visibility": "off"
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "poi.park",
+        "stylers": [{"visibility": "on"}]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "road.highway",
+        "stylers": [{"visibility": "simplified"}]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [{"visibility": "off"}]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [{"visibility": "off"}]
       }
     ]
-  },
-  {
-    "featureType": "poi",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "visibility": "on"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "stylers": [
-      {
-        "visibility": "simplified"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  }
-]
   });
+  
   fotball = new google.maps.Marker({
-      position: {lat: 60.390222, lng: 5.321039},
-      map: map,
-      store_id: 'fotball',
-    });
+    position: {lat: 60.390222, lng: 5.321039},
+    map: map,
+    store_id: 'fotball',
+  });
 
   bryggen = new google.maps.Marker({
     position: {lat: 60.395271, lng: 5.325236},
     map: map,
     store_id: 'bryggen',
   });
+}
+
+function getLocation() {
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+  'Error: The Geolocation service failed.' :
+  'Error: Your browser doesn\'t support geolocation.');
+}
+
+function centerMap() {
+  map.setCenter(center);
 }
