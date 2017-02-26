77076e25a59fd9d6d375bf328d34ae04cdc7a784
ohstopityou <thomasovik@gmail.com>
2017-02-26 22:09:26 +0100

    more minimalistic map

--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,66 @@
 function initMap() {
-  var uluru = {lat: 60.3907, lng: 5.319};
+  var uluru = {lat: 60.3907, lng: 5.322};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 15,
     center: uluru,
     zoomControl: false,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
+    styles: [
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
   });
   fotball = new google.maps.Marker({
       position: {lat: 60.390222, lng: 5.321039},
